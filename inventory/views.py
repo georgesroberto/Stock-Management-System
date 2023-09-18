529323ac800ab11b4616622337e2c7c1b8b415f3
unknown <georgesroberto21@gmail.com>
2023-09-18 10:22:38 +0300

    Updated delete_item page

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import HttpResponse, render, redirect, get_object_or_404
 from .models import Stock
 from .forms import StockCreateForm, StockSearchForm, StockUpdateForm
+from django.contrib import messages
+import csv
 
 def home(request):
     title = 'Welcome: This is the Home Page'
@@ -16,6 +18,16 @@ def list_items(request):
         category = form['category'].value()
         item_name = form['item_name'].value()
         stockList = Stock.objects.filter(category__icontains=category, item_name__icontains=item_name)
+
+        if form['export_to_CSV'].value() == True:
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename="List of stock.csv"'
+            writer = csv.writer(response)
+            writer.writerow(['CATEGORY', 'ITEM NAME', 'QUANTITY'])
+            instance = stockList
+            for stock in instance:
+                writer.writerow([stock.category, stock.item_name, stock.quantity])
+            return response
     
     context = {
         "items": stockList,
@@ -29,6 +41,7 @@ def add_items(request):
     form = StockCreateForm(request.POST or None)
     if form.is_valid():
         form.save()
+        messages.success(request, 'Successfully Saved')
         return redirect('list_items')  # Use the named URL 'list_items'
 
     context = {
@@ -39,25 +52,27 @@ def add_items(request):
     return render(request, 'add_items.html', context)
 
 def update_item(request, pk):
-    stock_instance = get_object_or_404(Stock, id=pk)
-    form = StockUpdateForm(instance=stock_instance)
+    stockList = get_object_or_404(Stock, id=pk)
+    form = StockUpdateForm(instance = stockList)
 
     if request.method == 'POST':
-        form = StockUpdateForm(request.POST, instance=stock_instance)
+        form = StockUpdateForm(request.POST, instance=stockList)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Successfully Updated')
             return redirect('list_items')  # Use the named URL 'list_items'
 
     context = {
         'form': form,
     }
-    return render(request, 'update_item.html', context)
+    return render(request, 'add_items.html', context)
 
 def delete_item(request, pk):
     stock_instance = get_object_or_404(Stock, id=pk)
     
     if request.method == 'POST':
         stock_instance.delete()
-        return redirect('list_items')  # Use the named URL 'list_items'
+        messages.success(request, 'Successfully Deleted')
+        return redirect('/list_items')  # Use the named URL 'list_items'
     
     return render(request, 'delete_item.html')
