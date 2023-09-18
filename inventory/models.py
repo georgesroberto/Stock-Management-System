b5c1fbc7c2439d3ee44b81efc5c11e70c62fb84f
unknown <georgesroberto21@gmail.com>
2023-09-18 10:23:13 +0300

    Uncommented CSV file

--- inventory/models.py
+++ inventory/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 
 # Create your models here.
 class Stock(models.Model):
-	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
 	item_name = models.CharField(max_length=50, blank=True, null=False)
 	quantity = models.IntegerField(default='0', blank=False, null=False)
 	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
@@ -27,7 +27,6 @@ class Stock(models.Model):
 	created_by = models.CharField(max_length=50, blank=True, null=True)
 	reorder_level = models.IntegerField(default='0', blank=True, null=True)
 	last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-	export_to_CSV = models.BooleanField(default=False)
-
+	export_to_CSV = models.BooleanField(default=True)
 	def __str__(self):
 		return self.item_name + ' ' + str(self.quantity)
