e137ff7ff37a86a89ebb6825e3b67f2f7a88cfe3
unknown <georgesroberto21@gmail.com>
2023-09-19 06:52:34 +0300

    Including History Model

--- inventory/models.py
+++ inventory/models.py
@@ -28,5 +28,21 @@ class Stock(models.Model):
 	last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 	date = models.DateTimeField(auto_now_add=False, auto_now=False)
 	export_to_CSV = models.BooleanField(default=True)
+	
 	def __str__(self):
 		return self.item_name + ' ' + str(self.quantity)
+	
+class StockHistory(models.Model):
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+	item_name = models.CharField(max_length=50, blank=True, null=True)
+	quantity = models.IntegerField(default='0', blank=True, null=True)
+	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
+	receive_by = models.CharField(max_length=50, blank=True, null=True)
+	issue_quantity = models.IntegerField(default='0', blank=True, null=True)
+	issue_by = models.CharField(max_length=50, blank=True, null=True)
+	issue_to = models.CharField(max_length=50, blank=True, null=True)
+	phone_number = models.CharField(max_length=50, blank=True, null=True)
+	created_by = models.CharField(max_length=50, blank=True, null=True)
+	reorder_level = models.IntegerField(default='0', blank=True, null=True)
+	last_updated = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
+	timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
