c5fa80568a78807394d1ad21aa724779424a4e54
unknown <georgesroberto21@gmail.com>
2023-09-25 23:37:51 +0300

    Cleaned form category

--- inventory/forms.py
+++ inventory/forms.py
@@ -6,21 +6,24 @@ class StockCreateForm(forms.ModelForm):
         model = Stock
         fields = ['category', 'item_name', 'quantity']
 
-    def clean_category(self):
-        category = self.cleaned_data.get('category')
-        if not category:
-            raise forms.ValidationError('This field is required')
+from django import forms
+from .models import Stock
 
-        # Check if the category already exists in the database
-        if Stock.objects.filter(category=category).exists():
-            raise forms.ValidationError(str(category) + '  already exits')
-        return category
+class StockCreateForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = ['category', 'item_name', 'quantity']
 
     def clean_item_name(self):
         item_name = self.cleaned_data.get('item_name')
-        if not item_name:
-            raise forms.ValidationError('This field is required')
-        return item_name
+        
+        # Check if an item with the same name already exists in the database
+        if Stock.objects.filter(item_name=item_name).exists():
+            raise forms.ValidationError('An item with this name already exists.')
+
+        return item_name  # Return the cleaned value
+
+    
 
 class StockSearchForm(forms.ModelForm):
    export_to_CSV = forms.BooleanField(required = False)
