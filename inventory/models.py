74886ef1d3518ff64eed133201f7c51645534b6c
unknown <georgesroberto21@gmail.com>
2023-09-25 23:36:23 +0300

    Setting name value to null

--- inventory/models.py
+++ inventory/models.py
@@ -33,7 +33,7 @@ class Stock(models.Model):
 	
 class StockHistory(models.Model):
 	category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
-	item_name = models.CharField(max_length=50, blank=True, null=True)
+	item_name = models.CharField(max_length=50, blank=True, null=False)
 	quantity = models.IntegerField(default='0', blank=True, null=True)
 	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
 	receive_by = models.CharField(max_length=50, blank=True, null=True)
