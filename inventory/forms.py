d8c39e81b19add1da97324435fcd738b864f67aa
unknown <georgesroberto21@gmail.com>
2023-10-09 20:40:47 +0300

    Form received

--- inventory/forms.py
+++ inventory/forms.py
@@ -6,24 +6,13 @@ class StockCreateForm(forms.ModelForm):
         model = Stock
         fields = ['category', 'item_name', 'quantity']
 
-from django import forms
-from .models import Stock
-
-class StockCreateForm(forms.ModelForm):
-    class Meta:
-        model = Stock
-        fields = ['category', 'item_name', 'quantity']
-
     def clean_item_name(self):
         item_name = self.cleaned_data.get('item_name')
-        
-        # Check if an item with the same name already exists in the database
+
         if Stock.objects.filter(item_name=item_name).exists():
             raise forms.ValidationError('An item with this name already exists.')
 
-        return item_name  # Return the cleaned value
-
-    
+        return item_name  
 
 class StockSearchForm(forms.ModelForm):
    export_to_CSV = forms.BooleanField(required = False)
@@ -48,7 +37,6 @@ class IssueForm(forms.ModelForm):
 	class Meta:
 		model = Stock
 		fields = ['issue_quantity', 'issue_to']
-
 
 class ReceiveForm(forms.ModelForm):
 	class Meta:
